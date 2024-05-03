a66302aa672caa204db7b2bb8f48bd452babc1a8
siwuzhifei <siwuzhifei@gmail.com>
2024-05-04 03:11:11 +1000

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,42 @@
+<?php
+// use to control login success/fail
+$is_invalid = false;
+
+// Create connection
+$connnection = new mysqli("localhost", "root", "", "fastfood_xc");
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+if($connnection->connect_error){
+ die("Connection failed: " . $connnection->connect_error);
+} 
+    // read the row of the selected client from database table
+    //$sql = "SELECT * FROM staff where email='$email'";
+    // to avoid a sql injection attack, we should use the following:
+    $sql = sprintf("SELECT * FROM staff WHERE email = '%s'", $connnection->real_escape_string($email));
+    $stmt_result = $connnection->query($sql);
+    if($stmt_result->num_rows > 0){
+        $data = $stmt_result->fetch_assoc();
+        if($password === $data['password']){
+            session_start();
+            $_SESSION['staffID'] = $data['staffID'];
+            $_SESSION['name'] = $data['name'];
+            $_SESSION['email'] = $data['email'];
+            $_SESSION['roleID'] = $data['roleID'];
+
+            header("Location: /XCfastfood/index2.php");
+            exit;
+        
+        } 
+    }
+// if we reach this point, form is submitted, but email or password is invalid
+    $is_invalid = true;
+}
+ 
+?>
+
 <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -15,6 +54,9 @@
                         <div class="card-header text-center bg-primary text-white">
                             <h2>Login Form</h2>
                         </div>
+                        <?php if ($is_invalid) : ?>
+                            <em>Invalid Login</em>
+                            <?php endif; ?>
                         <div class="card-body">
                             <form action="login.php" method="POST">
                                 <div class="form-group">
@@ -35,48 +77,3 @@
         </div>
     </body>
 </html>
-    
-<?php
-// use to control login success/fail
-$is_invalid = false;
-
-// Create connection
-$connnection = new mysqli("localhost", "root", "", "fastfood_xc");
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-
-if($connnection->connect_error){
- die("Connection failed: " . $connnection->connect_error);
-} else {
-    // read the row of the selected client from database table
-    //$sql = "SELECT * FROM staff where email='$email'";
-    // to avoid a sql injection attack, we should use the following:
-    $sql = sprintf("SELECT * FROM staff WHERE email = '%s'", $connnection->real_escape_string($email));
-    $stmt_result = $connnection->query($sql);
-    if($stmt_result->num_rows > 0){
-        $data = $stmt_result->fetch_assoc();
-        if($password === $data['password']){
-            session_start();
-            $_SESSION['staffID'] = $data['staffID'];
-            $_SESSION['name'] = $data['name'];
-            $_SESSION['email'] = $data['email'];
-            $_SESSION['roleID'] = $data['roleID'];
-
-            header("Location: /XCfastfood/index2.php");
-            exit;
-        
-        } else {
-            echo "Invalid email or password";
-        }
-    } else {
-        echo "Invalid email or password";
-    }
-}
-// if we reach this point, form is submitted, but email or password is invalid
-$is_invalid = true;
-
-}
-?>
