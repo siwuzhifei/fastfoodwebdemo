59c4981b059ad113ae629a1d4f79be2753ab9ec5
siwuzhifei <siwuzhifei@gmail.com>
2024-05-12 17:46:05 +1000

    Create Staff Availability Delete function

---
+++ availDelete.php
@@ -0,0 +1,28 @@
+<?php
+
+
+ // Create connection
+ $connnection = new mysqli("localhost", "root", "", "fastfood_xc");
+
+if ($connnection->connect_error) {
+    die("Connection failed: " . $connnection->connect_error);
+}
+
+if (isset($_POST['AvailableDelete-btn'])) {
+    $selectedValues = $_POST['availability_delete'];
+    $extractvalue = implode(",", $selectedValues);
+    echo $extractvalue;
+    $sql = "Delete from availability 
+    WHERE AvailabilityID IN ($extractvalue)";
+    $result = $connnection->query($sql);
+
+    if ($result) {
+        $_SESSION['message'] = "Record deleted successfully";
+        header("Location: /XCfastfood/availability2.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $connnection->error;
+    }
+}
+$connnection->close();
+
+?>
